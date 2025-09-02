76e3b45fe228d452a3b5999f7d529a903fa41bd7
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-02 10:56:52 +0530

    Add inverted half pyramid with numbers pattern in Java

---
+++ InvertedHalfPyramidWithNumber.java
@@ -0,0 +1,16 @@
+public class InvertedHalfPyramidWithNumber {
+    public static void InvertedHalfPyramidWithNumber(int n) {
+        // Outer
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        InvertedHalfPyramidWithNumber(5);
+    }
+
+}
